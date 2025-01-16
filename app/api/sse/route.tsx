1c2327e5b65bbe39b5ccd8f6dbfd405056d7b2a5
Victor Fuentes <victorp.fuentes@gmail.com>
2025-01-16 12:41:51 -0600

    convert to prisma, remove old db stuff

--- app/api/sse/route.tsx
+++ app/api/sse/route.tsx
@@ -1,26 +1,42 @@
-import { db } from "../../../lib/db";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function GET(request: any) {
   const stream = new ReadableStream({
     async start(controller) {
       const intervalId = setInterval(async () => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const [rows] = await db.query("SELECT * FROM scoreboard LIMIT 1");
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const newRows: any = rows;
-        // console.log({ response: newRows[0] });
-        controller.enqueue(`data: ${JSON.stringify(newRows[0])}\n\n`);
+        try {
+          // Fetch scoreboard data
+          const scoreboard = await prisma.scoreboard.findFirst();
+
+          // Handle the case where no data is found
+          if (!scoreboard) {
+            controller.enqueue(
+              `data: ${JSON.stringify({ message: "Scoreboard not found" })}\n\n`
+            );
+          } else {
+            controller.enqueue(`data: ${JSON.stringify(scoreboard)}\n\n`);
+          }
+        } catch (error) {
+          console.error("Error fetching scoreboard data:", error);
+          controller.enqueue(
+            `data: ${JSON.stringify({ message: "Internal Server Error" })}\n\n`
+          );
+        }
       }, 1000);
 
-      request.signal.addEventListener("abort", () => {
+      request.signal.addEventListener("abort", async () => {
         clearInterval(intervalId);
         controller.close();
+        await prisma.$disconnect();
       });
     },
   });
 
-  return new Response(stream, {
+  return new NextResponse(stream, {
     headers: {
       "Content-Type": "text/event-stream",
       "Cache-Control": "no-cache",
