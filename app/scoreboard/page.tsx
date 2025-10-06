d837db87d46dc878a8eeee44761995993a46fa8d
ricem9876 <44766410+ricem9876@users.noreply.github.com>
2025-10-05 20:40:53 -0500

    Update page.tsx

--- app/scoreboard/page.tsx
+++ app/scoreboard/page.tsx
@@ -217,11 +217,11 @@ function ScoreboardContent() {
       boxShadow={`${isRunning ? "inset 0 0 0 20px rgba(6, 251, 6, 0.5)" : " inset 0 0 0 10px rgba(250,156,28,0.8)"}`}
     >
       <Box
-        w={"350px"}
+        w={"600px"}
         height="350px"
         // background="red"
         position="fixed"
-        top="30px"
+        top="20px"
         left={"50px"}
         // backgroundImage={sponsorImage.src}
         borderRadius={"10px"}
